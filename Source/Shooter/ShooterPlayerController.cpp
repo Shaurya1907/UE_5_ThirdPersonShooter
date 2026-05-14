6997dd5e1c7eccc2daa6572cefd7bd24f73973e3
Shaurya Goyal <shauryagoyal1907@gmail.com>
2026-05-15 01:46:58 +0530

    Added HUD widget and animation blueprints

--- Source/Shooter/ShooterPlayerController.cpp
+++ Source/Shooter/ShooterPlayerController.cpp
@@ -31,6 +31,12 @@ void AShooterPlayerController::BeginPlay()
 		}
 
 	}
+
+	HUDWidget = CreateWidget<UHUDWidget>(this, HUDWidgetClass);
+	if(HUDWidget)
+	{
+		HUDWidget->AddToViewport();
+	}
 }
 
 void AShooterPlayerController::SetupInputComponent()
